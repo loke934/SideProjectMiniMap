0fb025c023c1468010a8c2478d37dd3e77e0d06e
Emilia Lokrantz <76095991+loke934@users.noreply.github.com>
2023-01-06 13:16:27 +0100

    Delete UIMiniMap.cpp

--- UIMiniMap/UIMiniMap.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UIMiniMap.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UIMiniMap, "UIMiniMap" );
- 
